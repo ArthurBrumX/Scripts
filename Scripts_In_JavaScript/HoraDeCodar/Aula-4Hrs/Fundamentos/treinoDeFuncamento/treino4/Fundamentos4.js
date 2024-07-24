9ac97c36df4a56eec0d47c87ab87fd8654c07992
Arthur Brum Xavier <98411287+ArthurBrumX@users.noreply.github.com>
2024-07-24 15:15:35 -0400

    UP

---
+++ Scripts_In_JavaScript/HoraDeCodar/Aula-4Hrs/Fundamentos/treinoDeFuncamento/treino4/Fundamentos4.js
@@ -0,0 +1,53 @@
+// Fundamentos
+
+var minhaVariavel = "Olá, Mundo!";
+console.log(minhaVariavel);
+
+//-----------------------------------------------------------------------------------------------
+
+// Variaveis e Tipos De Dados
+
+console.log("Variaveis e Tipos De Dados");
+
+var meuNumero = 10;
+console.log(meuNumero);
+console.log(meuNumero + 5);
+console.log(typeof meuNumero);
+
+var booleano = true;
+var meuObjeto = {};
+var meuArray = [];
+var meuNull = null;
+var meuUndefined = undefined;
+
+console.log("Isso é um: ",booleano);
+console.log("Isso é um: ",meuObjeto);
+console.log("Isso é um: ",meuArray);
+console.log("Isso é um: ",meuNull);
+console.log("Isso é um: ",meuUndefined);
+
+
+var x = 30;
+let y = 10;
+const z = 5;
+
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
+//-----------------------------------------------------------------------------------------------
+
+console.log("Operadores Aritmeticos!");
+
+console.log(x + y);
+console.log(x * y);
+console.log(x / y);
+console.log(x - y);
+
+//-----------------------------------------------------------------------------------------------
+
+console.log("Operadores de Comparacao!");
+
+console.log(x = y);
+console.log(x == y);
+console.log(x === y);
+console.log(x !== y);
